60013cea9d166a1a61abebac03494620c4717062
shmy <2668395493@qq.com>
2022-04-26 14:41:10 +0800

    main

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ var express = require('express')
 
 var app = express()
 
+app.use('/node_modules',express.static('./node_modules'))
+app.use('/public/',express.static('./public/'))
+
 app.engine('html',require('express-art-template'))
 
 app.get('/',function(req,res){
-    res.render('index.html')
+    res.render('index.html',{
+        fruits:[
+            '苹果',
+            '香蕉',
+            '句子',
+            '西瓜',
+        ]
+    })
 })
 
 app.listen(3000,function(){
